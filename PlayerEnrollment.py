c1a62482535cd0b929225744c992510c75374489
ysenlik <ysenlik@gmail.com>
2018-11-20 00:56:27 +0300

    PlayerEnrollment added

---
+++ PlayerEnrollment.py
@@ -0,0 +1,9 @@
+print("Player Enrollment Program: ")
+
+name=input("Enter player's name: ")
+surname=input("Enter player' s surname: ")
+team=input("Enter player's team: ")
+
+information=[name,surname, team];
+
+print("Player Name: {}\n Player Surname: {}\n Player Team: {}".format(information[0],information[1], information[2]))
